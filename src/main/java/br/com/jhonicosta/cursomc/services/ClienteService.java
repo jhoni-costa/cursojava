6bdfbf654a72764caf62695ffa68349182f316d2
Jhoni Costa <jhoni_costa@outlook.com>
2018-07-20 17:29:16 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/br/com/jhonicosta/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package br.com.jhonicosta.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.jhonicosta.cursomc.domain.Cliente;
+import br.com.jhonicosta.cursomc.repositories.ClienteRepository;
+import br.com.jhonicosta.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repository;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
