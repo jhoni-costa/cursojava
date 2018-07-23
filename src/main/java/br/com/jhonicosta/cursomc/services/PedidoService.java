3dfd326ffcbc3d39776cc357c3f06e1362c442e6
Jhoni Costa <jhoni_costa@outlook.com>
2018-07-23 16:06:28 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/br/com/jhonicosta/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package br.com.jhonicosta.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.jhonicosta.cursomc.domain.Pedido;
+import br.com.jhonicosta.cursomc.repositories.PedidoRepository;
+import br.com.jhonicosta.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repository;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
