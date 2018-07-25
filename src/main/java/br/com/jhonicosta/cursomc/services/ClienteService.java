a84ec09596a38e63976d487c09b109f28a27fa08
Jhoni Costa <jhoni_costa@outlook.com>
2018-07-25 16:11:34 -0300

    Ajustando a deleção de cliente

--- src/main/java/br/com/jhonicosta/cursomc/services/ClienteService.java
+++ src/main/java/br/com/jhonicosta/cursomc/services/ClienteService.java
@@ -57,7 +57,7 @@ public class ClienteService {
 		try {
 			repository.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possivel excluir porque ha entidades relacionadas");
+			throw new DataIntegrityException("Não é possivel excluir porque há pedidos relacionados");
 		}
 	}
 
