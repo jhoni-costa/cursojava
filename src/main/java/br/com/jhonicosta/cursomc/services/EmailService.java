f81e6c2d795c6e6d2ec500a56146bea9f735ad09
Jhoni Costa <jhoni_costa@outlook.com>
2018-08-03 17:34:19 -0300

    Email HTML

--- src/main/java/br/com/jhonicosta/cursomc/services/EmailService.java
+++ src/main/java/br/com/jhonicosta/cursomc/services/EmailService.java
@@ -1,5 +1,8 @@
 package br.com.jhonicosta.cursomc.services;
 
+
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import br.com.jhonicosta.cursomc.domain.Pedido;
@@ -9,4 +12,8 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendMail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
--- src/main/java/br/com/jhonicosta/cursomc/services/SmtpEmailService.java
+++ src/main/java/br/com/jhonicosta/cursomc/services/SmtpEmailService.java
@@ -1,22 +1,35 @@
 package br.com.jhonicosta.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
-public class SmtpEmailService extends AbstractEmailService{
-	
+public class SmtpEmailService extends AbstractEmailService {
+
 	@Autowired
 	private MailSender mailSender;
 	
+	@Autowired
+	private JavaMailSender javaMailSender;
+
 	private static final Logger LOG = LoggerFactory.getLogger(SmtpEmailService.class);
-	
+
 	@Override
 	public void sendMail(SimpleMailMessage msg) {
 		LOG.info("Envio de Email");
 		mailSender.send(msg);
+		LOG.info("Email enviado");
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Envio de Email");
+		javaMailSender.send(msg);
 		LOG.info("Email enviado");
 	}
 
