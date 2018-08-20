8b250d127e6e435004864e9a6900ef18ea6db76b
Jhoni Costa <jhoni_costa@outlook.com>
2018-08-20 17:53:43 -0300

    Teste

--- src/main/java/br/com/jhonicosta/cursomc/services/AbstractEmailService.java
+++ src/main/java/br/com/jhonicosta/cursomc/services/AbstractEmailService.java
@@ -35,14 +35,13 @@ public abstract class AbstractEmailService implements EmailService{
 
 	protected SimpleMailMessage prepareSimpleMailMessageFromPedido(Pedido obj) {
 		
-		SimpleMailMessage msg = new SimpleMailMessage();
-		msg.setTo(obj.getCliente().getEmail());
-		msg.setFrom(sender);
-		msg.setSubject("Pedido Confirmado! Código: " + obj.getId());
-		msg.setSentDate(new Date(System.currentTimeMillis()));
-		msg.setText(obj.toString());
-		
-		return msg;
+		SimpleMailMessage sm = new SimpleMailMessage();
+		sm.setTo(obj.getCliente().getEmail());
+		sm.setFrom(sender);
+		sm.setSubject("Pedido confirmado! Código: " + obj.getId());
+		sm.setSentDate(new Date(System.currentTimeMillis()));
+		sm.setText(obj.toString());
+		return sm;
 	}
 	
 	protected String htmlFromTemplatePedido(Pedido obj) {
